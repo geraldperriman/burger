fbe26e204004c40771a5cdab23142575019bfc3d
Gerald Perriman <geraldperriman@gmail.com>
2017-08-14 20:10:38 -0400

    b

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sunrise Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
